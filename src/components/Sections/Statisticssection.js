7b9909e8bb7794db76c9629817e25abe627c80a1
MaksNyzhnov <123084582+MaksNyzhnov@users.noreply.github.com>
2023-08-03 01:49:45 +0300

    made notification for zero feedbacks

--- src/components/Sections/Statisticssection.js
+++ src/components/Sections/Statisticssection.js
@@ -1,12 +1,33 @@
 import PropTypes from 'prop-types';
 import css from './Section.module.css';
 import Statistics from 'components/Statistics/Statistics';
+import Notification from 'components/Notification/Notification';
 
+// const StatisticsSection = ({ title, feedbacks }) => {
+//   console.log(feedbacks);
+//   return (
+//     <section className={css.section}>
+//       <h2 className={css.title}>{title}</h2>
+
+//       (feedbacks.good=== 0 || feedbacks.bad === 0 || feedbacks.neutral === 0) ?{' '}
+//       <Notification message="There is no feedback" /> :
+//       <Statistics feedbacks={feedbacks} />
+//     </section>
+//   );
+// };
 const StatisticsSection = ({ title, feedbacks }) => {
+  console.log(feedbacks);
   return (
     <section className={css.section}>
       <h2 className={css.title}>{title}</h2>
-      <Statistics feedbacks={feedbacks} />
+
+      {feedbacks.good === 0 &&
+      feedbacks.bad === 0 &&
+      feedbacks.neutral === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Statistics feedbacks={feedbacks} />
+      )}
     </section>
   );
 };
