a8cc5a491109858d8540413ad0a1253f6407f2af
MaksNyzhnov <123084582+MaksNyzhnov@users.noreply.github.com>
2023-08-03 01:58:51 +0300

    Update Notification.js

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import css from './Notification.module.css';
 class Notification extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return <p className={css.notification__text}>{this.props.message}</p>;
   }
