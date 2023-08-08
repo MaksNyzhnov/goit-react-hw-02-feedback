6f4d8ceb3a0e5ce150ce5d21eef496ace9ef698a
MaksNyzhnov <123084582+MaksNyzhnov@users.noreply.github.com>
2023-08-09 01:17:44 +0300

    made refactoring for feedback options

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -2,44 +2,42 @@ import React from 'react';
 
 import css from './FeedbackOptions.module.css';
 
-class FeedbackOptions extends React.Component {
-  onButtonClick = event => {
-    this.props.updateState(`${event.target.textContent.toLowerCase()}`);
+const FeedbackOptions = ({ updateState }) => {
+  const onButtonClick = event => {
+    updateState(`${event.target.textContent.toLowerCase()}`);
   };
 
-  render() {
-    return (
-      <ul className={css.feedback__list}>
-        <li className={css.feedback__list__item}>
-          <button
-            type="button"
-            className={css.feedback__button}
-            onClick={this.onButtonClick}
-          >
-            Good
-          </button>
-        </li>
-        <li className={css.feedback__list__item}>
-          <button
-            type="button"
-            className={css.feedback__button}
-            onClick={this.onButtonClick}
-          >
-            Neutral
-          </button>
-        </li>
-        <li className={css.feedback__list__item}>
-          <button
-            type="button"
-            className={css.feedback__button}
-            onClick={this.onButtonClick}
-          >
-            Bad
-          </button>
-        </li>
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className={css.feedback__list}>
+      <li className={css.feedback__list__item}>
+        <button
+          type="button"
+          className={css.feedback__button}
+          onClick={onButtonClick}
+        >
+          Good
+        </button>
+      </li>
+      <li className={css.feedback__list__item}>
+        <button
+          type="button"
+          className={css.feedback__button}
+          onClick={onButtonClick}
+        >
+          Neutral
+        </button>
+      </li>
+      <li className={css.feedback__list__item}>
+        <button
+          type="button"
+          className={css.feedback__button}
+          onClick={onButtonClick}
+        >
+          Bad
+        </button>
+      </li>
+    </ul>
+  );
+};
 
 export default FeedbackOptions;
